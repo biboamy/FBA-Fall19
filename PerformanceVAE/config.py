c7c21685b2db5c41e2998786326fb24bade4d140
ashispati <ashis.pati@gatech.edu>
2020-05-06 21:51:59 +0000

    add results for chunk size experiments

--- PerformanceVAE/config.py
+++ PerformanceVAE/config.py
@@ -1,10 +1,13 @@
+import torch
+torch.cuda.set_device(0)
+
 # path
 PATH_FBA_DILL = "/media/Data/saved_dill/"
 PATH_FBA_MIDI = "/media/Data/fall19/data/midi/"
 PATH_FBA_MTX = "/media/Data/fall19/data/matrix/"
 PATH_FBA_SPLIT = "/media/Data/split_dill/"
 
-band = "symphonic"
+band = "middle"
 split = "new"  # old: 2013~2015, new: 2013~2018
 
 data_all_pc = "{}_2_pc_6_fix.dill"
@@ -18,7 +21,7 @@ midi_aligned_s = "{}_2_midi_aligned_s_6.dill"
 feat = 'pitch contour'  # fixed
 midi_op = 'aligned_s'  # fixed
 model_choose = "PerformanceEncoder"  # 'PerformanceVAE', 'PerformanceEncoder', 'PCConvNet'
-score_choose = 0  # 0: musicality, 1: note acc., 2: rhythmic acc. 3: tone quality
+score_choose = 2  # 0: musicality, 1: note acc., 2: rhythmic acc. 3: tone quality
 
 # training parameters
 batch_size = 32
@@ -31,7 +34,7 @@ beta = 1.0
 
 process_collate = 'randomChunk'  # 'randomChunk', 'windowChunk', 'padding'
 sample_num = 2  # numbers of chunks # if choosing windowChunk, sample_num has to be 1
-chunk_size = 2000  # 1000 ~ 5 sec / 2000 ~ 10 sec
+chunk_size = 1000  # 1000 ~ 5 sec / 2000 ~ 10 sec
 
 # model parameters
 dropout_prob = 0.1
