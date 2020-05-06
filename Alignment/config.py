b381519d91fcb072329154eb34e4ef52bf8c3ae9
biboamy <biboamybibo@gmail.com>
2020-05-06 14:52:32 +0000

    update size

--- Alignment/config.py
+++ Alignment/config.py
@@ -4,7 +4,7 @@ PATH_FBA_MIDI = "/media/Data/fall19/data/midi/"
 PATH_FBA_MTX = "/media/Data/fall19/data/matrix/"
 PATH_FBA_SPLIT = "/media/Data/split_dill/"
 
-band = "middle" #symphonic middle
+band = "symphonic" #symphonic middle
 split = "new" # old: 2013~2015, new: 2013~2018
 
 data_all_pc = "{}_2_pc_6_fix.dill"
@@ -19,7 +19,7 @@ midi_aligned_s = "{}_2_midi_aligned_s_6.dill"
 feat = 'pitch contour' # fixed
 midi_op = 'aligned_s' # fixed
 model_choose = 'CNN' # CNN CRNN
-score_choose = 2 # 0: musicality, 1: note acc., 2: rhythmic acc. 3: tone quality
+score_choose = 0 # 0: musicality, 1: note acc., 2: rhythmic acc. 3: tone quality
 normalize = True # WARNING
 
 # training parameters
@@ -32,7 +32,7 @@ lr = 0.05
 loss_func = 'Similarity'
 process_collate = 'randomChunk' # 'randomChunk', 'windowChunk', 'padding'
 sample_num = 2 # numbers of chunks # if choosing windowChunk, sample_num has to be 1
-chunk_size = 2000 # 1000 ~ 5 sec / 2000 ~ 10 sec
+chunk_size = 1000 # 1000 ~ 5 sec / 2000 ~ 10 sec
 
 # evaluation parameters
 overlap_flag = False
--- Alignment/eval.py
+++ Alignment/eval.py
@@ -74,7 +74,7 @@ def main():
 
     eval_metrics = dict()
     for i in range(0,10):
-        model_name = '202052/Similarity_batch32_lr0.05_midialigned_s_{}_sample2_chunksize2000_{}_{}{}_score{}_NORM_' \
+        model_name = '202055/Similarity_batch32_lr0.05_midialigned_s_{}_sample2_chunksize1000_{}_{}{}_score{}_NORM_' \
                      .format(process_collate, model_choose, band, split, score_choose) + str(i)
 
         #model_name = model_choose + '_' + str(i)
@@ -116,7 +116,7 @@ def main():
         sum(test_metrics_Flute) / len(test_metrics_Flute)
     )
 
-    model_n = "Similarity_batch32_lr0.05_midialigned_s_{}_sample2_chunksize2000_{}".format(process_collate, model_choose)
+    model_n = "Similarity_batch32_lr0.05_midialigned_s_{}_sample2_chunksize1000_{}".format(process_collate, model_choose)
     results_dir = './results'
     results_fp = os.path.join(
         results_dir,
